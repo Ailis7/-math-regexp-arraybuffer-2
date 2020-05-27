815abb07acdedbbc8ba27b099a51d32aad96f30c
Petr Brovkin <bropet@mail.ru>
2020-05-27 15:28:00 +0300

    final

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+// файл сборщик
+import Character from './classes/Character'; // основной прототип персонажей
+import Bowman from './classes/Bowman';
+import Swordsman from './classes/Swordsman';
+import Magician from './classes/Magician';
+import Undead from './classes/Undead';
+import Zombie from './classes/Zombie';
+import Daemon from './classes/Daemon';
+import Team from './classes/Team'; // распределение по командам
+import ErrorRepository from './classes/ErrorRepository'; // ошибки
+import Settings from './classes/Settings'; // настройки игры
+import Validator from './classes/Validator'; // проверка имя пользователя
